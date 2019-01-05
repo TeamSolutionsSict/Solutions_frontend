bc4f0d6ed649f925533de40b978f4975c5f3fc7e
nvchau <46041824+nvchau@users.noreply.github.com>
2019-01-05 23:41:43 +0700

    Update add_question.blade.php

--- resources/views/page/add_question.blade.php
+++ resources/views/page/add_question.blade.php
@@ -1,9 +1,10 @@
 @extends('page.master')
 
-@section('wrap-content')
-<div class="breadcrumbs">
-    
-</div>
+
+@section ('section-warp')
+    <div class="section-warp ask-me">
+
+    </div><!-- End section-warp -->
 @endsection
 
 @section('content')
@@ -12,7 +13,7 @@
         <div class="boxedtitle page-title"><h2>Ask Question</h2></div>
 
         <div class="form-style form-style-3" id="question-submit">
-            <form action="{{ route('post.AddQuestion') }}" method="POST">
+            <form action="" method="POST">{{-- {{ route('post.AddQuestion') }} --}}
                 {{ csrf_field() }}
                 <div class="form-inputs clearfix">
                     <p>
@@ -32,7 +33,7 @@
                         <textarea id="editor1" aria-required="true" name="content"></textarea>
                     </p>
                 </div>
-                <script src="public/page/ckeditor/ckeditor.js"></script>
+                <script src="{{asset('page/ckeditor/ckeditor.js')}}"></script>
                 <script> CKEDITOR.replace('editor1'); </script>
                 <p class="form-submit">
                     <input type="submit" id="publish-question" value="Publish Your Question" class="button color small submit">
