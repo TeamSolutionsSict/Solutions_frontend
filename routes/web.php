a7e9b2ab5aeaa645df0d6c9788e4922df83fd61c
nvchau <46041824+nvchau@users.noreply.github.com>
2019-01-05 23:48:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,26 @@
 |
 */
 
+//Home
 Route::get('home',['as'=>'get.Home','uses'=>'pageController@getHome']);
 
+//Điều khoản (terms)
 Route::get('terms',['as'=>'get.Terms','uses'=>'pageController@getTerms']);
 
+//Add question
 Route::get('add-question',['as'=>'get.AddQuestion','uses'=>'pageController@getAddQuestion']);
-Route::post('add-question',['as'=>'post.AddQuestion','uses'=>'pageController@postAddQuestion']);
+//Route::post('add-question',['as'=>'post.AddQuestion','uses'=>'pageController@postAddQuestion']);
 
+//Question details
 Route::get('question-details',['as'=>'get.QuestionDetails','uses'=>'pageController@getQuestionDetails']);
+//Route::get('question-details{id}',['as'=>'get.QuestionDetails','uses'=>'pageController@getQuestionDetails']);
 
+//Profile của người đang đăng nhập
+Route::get('profile',['as'=>'get.Profile','uses'=>'pageController@getProfile']);
+
+//User (xem thông tin của người dùng khác)
 Route::get('user',['as'=>'get.User','uses'=>'pageController@getUser']);
 
+//Contact
 Route::get('contact',['as'=>'get.Contact','uses'=>'pageController@getContact']);
+
